cffe820e667ba0ac0dbe35f89bcc3ac1dc545ee7
Claudia Tang <ct3008@barnard.edu>
2024-11-26 00:24:19 -0500

    SSL issues persist

--- worker.py
+++ worker.py
@@ -16,14 +16,26 @@ print("REDIS URL: ", url)
 print("SEGMENT: ", url.hostname, url.port, url.password)
 
 # Configure Redis connection with SSL/TLS support and certificate validation disabled
+# redis_conn = redis.StrictRedis(
+#     host=url.hostname,
+#     port=url.port,
+#     password=url.password,
+#     ssl=False,
+#     ssl_cert_reqs=ssl.CERT_NONE  # Disable certificate validation for self-signed certificates
+# )
+
 redis_conn = redis.StrictRedis(
-    host=url.hostname,
-    port=url.port,
-    password=url.password,
-    db=0,
-    ssl=True,
-    ssl_cert_reqs=ssl.CERT_NONE  # Disable certificate validation for self-signed certificates
+    host='ec2-18-206-36-186.compute-1.amazonaws.com',
+    port=23840,
+    ssl=True,  # Enable SSL/TLS
+    ssl_certfile=None,  # Path to client certificate (if needed)
+    ssl_keyfile=None,   # Path to client key file (if needed)
+    ssl_ca_certs=None,  # Path to CA certificate if you're verifying the server's certificate
+    ssl_check_hostname=False  # This will skip verification of the server's hostname (useful if using a self-signed certificate)
 )
+
+redis_conn.ssl_context.verify_mode = ssl.CERT_NONE  # Disable certificate verification
+
 
 # Listen to the default queue
 if __name__ == "__main__":
